1c7e8da2143103ec329c8bdc067dd46111968339
belenfayaslamerce <belenfayaslamerce@9b404b30-beee-de0d-c69c-6a6f12e28b93>
2014-03-11 15:28:22 +0000

    vuelvo a añadir el código comentado

--- trunk/Factorial/src/fact/CalculaFactorial.java
+++ trunk/Factorial/src/fact/CalculaFactorial.java
@@ -16,7 +16,11 @@ public class CalculaFactorial {
 		}
 	}
 	}
-
+	
+	/*
+	 * double result; result=1; while (n>0) { result = result * n; n=n-1; }
+	 * return result; }
+	 */
 	
 	public static void main(String[] args) {
 		CalculaFactorial f = new CalculaFactorial();
