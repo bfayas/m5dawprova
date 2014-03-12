3ef4d7afedf33f57ecddf13e65f6f6ac4a058944
belenfayasb9 <belenfayasb9@9b404b30-beee-de0d-c69c-6a6f12e28b93>
2014-03-12 13:06:15 +0000

    importació inicial

---
+++ Operacions/src/multiplicar/tablaMultiplicar.java
@@ -0,0 +1,19 @@
+package multiplicar;
+import java.util.Scanner;
+
+public class tablaMultiplicar {
+
+	private static Scanner leer;
+	
+	public static void main(String[] args) {
+		int valor;
+	
+        leer=new Scanner(System.in);
+		System.out.println ("Introduce un numero entero");
+        valor=Integer.parseInt(leer.next());
+	  	System.out.println ("Tabla de multiplicar del numero " + valor);
+		for	(int i=0; i<=10; i++) {
+			System.out.println(valor + " x " + i + " = " + valor*i ); 
+		}
+	}
+}
