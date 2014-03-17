aa5c697af41351998723bf6876d16007a9e20cc5
belenfayasb9 <belenfayasb9@9b404b30-beee-de0d-c69c-6a6f12e28b93>
2014-03-17 12:02:35 +0000

    Importació inicial.

---
+++ Operacions/src/multiplicar/taulaMultiplicar.java
@@ -0,0 +1,13 @@
+package multiplicar;
+
+public class taulaMultiplicar {
+
+	public static void main(String[] args) {
+
+		System.out.println ("Taula de multiplicar del número " + "8");
+		for	(int i=1; i<=10; i++) {
+			System.out.println(8 + " x " + i + " = " + 8*i ); 
+		}
+	}
+	
+}
