9c8b7285892e5651cea9ec3955ac30d6437b8ff6
belenfayasb9 <belenfayasb9@9b404b30-beee-de0d-c69c-6a6f12e28b93>
2014-03-25 15:12:27 +0000

    des de zero

--- trunk/Operacions/src/multiplicar/taulaMultiplicar.java
+++ trunk/Operacions/src/multiplicar/taulaMultiplicar.java
@@ -4,8 +4,8 @@ public class taulaMultiplicar {
 
 	public static void main(String[] args) {
 
-		System.out.println ("Taula de multiplicar del número " + "8");
-		for	(int i=1; i<=10; i++) {
+		System.out.println ("Taula de multiplicar del numero " + "8");
+		for	(int i=0; i<=10; i++) {
 			System.out.println(8 + " x " + i + " = " + 8*i ); 
 		}
 	}
